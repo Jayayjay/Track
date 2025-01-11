1df3a3d27f4d8e904f59e47b91741489449d63af
Jonathan Vongchak <nexusbrainjr@gmail.com>
2025-01-11 08:15:52 +0100

    update: included apps to the admin panel

--- HVT_backend/athlete_records/admin.py
+++ HVT_backend/athlete_records/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import AthleteRecords
 
 # Register your models here.
+class AthleteRecordsAdmin (admin.ModelAdmin):
+    list_display = [
+        'athlete_name',
+        'athlete_mark',
+        'athlete_age',
+        'athlete_group',
+        'athlete_date',
+        'athlete_meet'
+    ]
+admin.site.register(AthleteRecords, AthleteRecordsAdmin)
--- HVT_backend/contact/admin.py
+++ HVT_backend/contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import ContactMessage
 # Register your models here.
+
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'email',
+        'subject',
+        'message',
+        'created_at',
+    ]
+    
+admin.site.register(ContactMessage, ContactMessageAdmin)
