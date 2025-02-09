11ecdeb8187de3abde7ff39157f43eb513ed9b0f
Jonathan Vongchak <nexusbrainjr@gmail.com>
2025-02-09 23:40:31 +0100

    fix: settings.py

--- HVT_backend/HVT_backend/settings.py
+++ HVT_backend/HVT_backend/settings.py
@@ -12,16 +12,17 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!cot2v5zsa)j3c3pq5@pg)st#xyv8)ec$lek=llan5j$79iyhn'
+SECRET_KEY =  os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
