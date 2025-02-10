84f2c5edd5fcdcce57774efdc07d60c9aee8dd3a
Jonathan Vongchak <nexusbrainjr@gmail.com>
2025-02-10 01:47:41 +0100

    fix:update

--- HVT_backend/HVT_backend/settings.py
+++ HVT_backend/HVT_backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY =  os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['jayayjay.pythonanywhere.com', '127.0.0.1:8000', '127.0.0.1', 'highvoltagetrack.com']
+ALLOWED_HOSTS = [ 'www.highvoltagetrack.com','highvoltagetrack.com']
 
 
 # Application definition
