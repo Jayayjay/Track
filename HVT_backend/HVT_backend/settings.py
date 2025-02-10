2ebac0a429b90555c22459fda586bf333761781c
Jonathan Vongchak <nexusbrainjr@gmail.com>
2025-02-10 15:45:29 +0100

    New changes des

--- HVT_backend/HVT_backend/settings.py
+++ HVT_backend/HVT_backend/settings.py
@@ -133,12 +133,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'athlete_records', 'static'),
-]
+print(BASE_DIR)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# STATICFILES_FINDERS = [
+#     'django.contrib.staticfiles.finders.FileSystemFinder',
+#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+# ]
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
