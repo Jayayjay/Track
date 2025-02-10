f66bfea8ecbca48dd6522c76ae6ca547474e791f
Jonathan Vongchak <nexusbrainjr@gmail.com>
2025-02-10 14:42:40 +0100

    fix: resolved multiple instances of the static dir

--- HVT_backend/HVT_backend/settings.py
+++ HVT_backend/HVT_backend/settings.py
@@ -25,9 +25,9 @@ load_dotenv()
 SECRET_KEY =  os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [ 'www.highvoltagetrack.com','highvoltagetrack.com']
+ALLOWED_HOSTS = [ 'www.highvoltagetrack.com','highvoltagetrack.com', '*']
 
 
 # Application definition
@@ -55,6 +55,7 @@ INSTALLED_APPS += EXTERNAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     # CORS Middleware
     'corsheaders.middleware.CorsMiddleware',
@@ -131,10 +132,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/athlete_records/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'athlete_records', 'static')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'athlete_records', 'static'),
 ]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
