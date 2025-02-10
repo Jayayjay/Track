27be6618fc0ad26fd0d4be84e5c53efd0845cfb4
Jonathan Vongchak <nexusbrainjr@gmail.com>
2025-02-10 15:56:04 +0100

    Debug false

--- HVT_backend/HVT_backend/settings.py
+++ HVT_backend/HVT_backend/settings.py
@@ -25,9 +25,9 @@ load_dotenv()
 SECRET_KEY =  os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [ 'www.highvoltagetrack.com','highvoltagetrack.com', '*']
+ALLOWED_HOSTS = [ 'www.highvoltagetrack.com','highvoltagetrack.com']
 
 
 # Application definition
